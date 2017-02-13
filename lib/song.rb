a4980a237ad30bc916621ee14124672b2d166af6
Your Name <you@example.com>
2017-02-12 21:53:22 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,72 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+
+
+  def self.all #contextually Song.call
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create # allows for song=Song.create
+    song = Song.new
+    song.save
+    song
+  end
+
+
+  def self.new_by_name(song_name)
+      song = Song.new
+      song.name = song_name
+      song
+  end
+
+  def self.create_by_name(name)
+    song= Song.new
+    song.name=name
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|song| song.name==song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name)|| self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+     split_file = filename.split(" - ")
+     artist_name = split_file[0]
+     song_name = split_file[1]
+
+     song = self.create
+     song.name = song_name.chomp(".mp3")
+     song.artist_name = artist_name
+     song
+   end
+
+   def self.create_from_filename(filename)
+     split_file = filename.split(" - ")
+     artist_name = split_file[0]
+     song_name = split_file[1]
+
+     song = self.create
+     song.name = song_name.chomp(".mp3")
+     song.artist_name = artist_name
+     song
+   end
+
+  def self.destroy_all #Song.destroy_all
+    self.all.clear
   end
 
 end
